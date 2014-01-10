122e3d8407971f5d3f756fcf3674dcc91b0960cb
hadley <h.wickham@gmail.com>
2014-01-10 05:53:43 -0600

    Add code and documentation

---
+++ R/code.R
@@ -0,0 +1,69 @@
+#' Check if RStudio is running?
+#' 
+#' @return \code{isRstudio} a boolean; \code{checkRstudio} an error message
+#'   if Rstudio is not running
+#' @export
+#' @examples
+#' isRstudio()
+#' \donttest{checkRstudio()}
+isRstudio <- function() {
+  identical(.Platform$GUI, "RStudio")
+}
+
+#' @rdname isRstudio
+#' @export
+checkRstudio <- function() {
+  if (!isRstudio()) stop("RStudio not running", call. = FALSE)
+}
+
+#' Call an Rstudio API function
+#' 
+#' This function will return an error if Rstudio is not running, or the 
+#' function is not available. If you want to fall back to different 
+#' behavour, use \code{\link{existsRstudio}}.
+#' 
+#' @param fname name of the Rstudio function to call. 
+#'   See \code{help(package = "Rstudio")}. For a complete list of functions
+#'   in the current API.
+#' @param ... Other arguments passed on to the function
+#' @export
+#' @examples
+#' if (isRstudio()) {
+#'   callApi("versionInfo")
+#' }
+callApi <- function(fname, ...) {
+  checkRstudio()
+  
+  if (!existsRstudio(fname, mode = "function")) {
+    stop("Function ", fname, " not found in Rstudio", call. = FALSE)
+  }
+  
+  f <- getRstudio(fname, mode = "function")
+  f(...)
+}
+
+#' Exists/get for Rstudio functions
+#' 
+#' These are specialised versions of \code{\link{get}} and \code{\link{exists}}
+#' that look in the rstudio package namespace. If Rstudio is not running,
+#' \code{existsRstudio} will return \code{FALSE}, and \code{getRstudio}
+#' will raise an error.
+#' 
+#' @param name name of object to look for
+#' @param ... other arguments passed on to \code{\link{exists}} and 
+#'   \code{\link{get}}
+#' @export
+#' @examples
+#' existsRstudio("viewer")
+#' \donttest{existsRstudio("viewer")}
+existsRstudio <- function(name, ...) {
+  if (!isRstudio()) return(FALSE)
+  exists(name, envir = asNamespace("rstudio"), ...)
+}
+
+#' @export
+#' @rdname existsRstudio
+getRstudio <- function(name, ...) {
+  checkRstudio()
+  get(name, envir = asNamespace("rstudio"), ...)
+}
