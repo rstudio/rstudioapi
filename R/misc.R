701799b58f3bf2ead99a183d70a5f88aaed42ed4
Kevin Ushey <kevinushey@gmail.com>
2015-12-02 21:17:21 -0800

    use 'encodeString()'

--- R/misc.R
+++ R/misc.R
@@ -6,7 +6,5 @@ formatText <- function(text, n = 20L, truncated = "<...>") {
   else
     paste(text, truncated)
 
-  # use capture.output and print.default to escape e.g. newlines
-  captured <- capture.output(print.default(result, quote = FALSE))
-  substring(captured, 5)
+  encodeString(result)
 }
