16a7796831dd5835e30aa9a0b4c071f388816936
JJ Allaire <jj@rstudio.org>
2016-02-09 12:44:20 -0500

    Revert "add sendToConsole function"
    This reverts commit 94d7a85923be464eda3bf7fbc28a657219a826f8.

--- R/stubs.R
+++ R/stubs.R
@@ -36,25 +36,3 @@ getActiveProject <- function() {
   callFun("getActiveProject")
 }
 
-
-#' Send Code to the R Console
-#'
-#' Send code to the R console and optionally execute it.
-#'
-#' @param code Character vector containing code to be executed.
-#' @param execute \code{TRUE} to execute the code immediately.
-#'
-#' @note The \code{sendToConsole} function was added in version 0.99.787 of RStudio.
-#'
-#' @examples
-#' \dontrun{
-#' rstudioapi::sendToConsole(".Platform", execute = TRUE)
-#' }
-#'
-#' @export
-sendToConsole <- function (code, execute = TRUE) {
-  callFun("sendToConsole", code, TRUE, execute, TRUE)
-}
-
-
-
