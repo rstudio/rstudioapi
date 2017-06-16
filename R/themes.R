791a898d0e081c3dd56bf2f5c089c3b5a4b2aaf9
Javier Luraschi <javierluraschi@hotmail.com>
2017-06-16 12:17:46 -0700

    add support for dark under getThemeInfo

--- R/themes.R
+++ R/themes.R
@@ -1,21 +1,52 @@
 #' Retrieve Themes
 #'
-#' Retrieves a list with themes information. Currently, `editor` as
-#' the theme used under the code editor and `global` as the global theme
-#' applied to the main user interface in RStudio.
+#' Retrieves a list with themes information. Currently, \code{editor} as
+#' the theme used under the code editor, \code{global} as the global theme
+#' applied to the main user interface in RStudio and \code{dark} when
+#' the user interface is optimized for dark themes.
 #'
 #' @export
 getThemeInfo <- function(title, message, url = NULL) {
   editor <- .rs.readUiPref("theme")
+
   global <- .rs.readUiPref("flat_theme")
+  global <- switch(
+    if (is.null(global)) "" else global,
+    alternate = "Sky",
+    default = "Modern",
+    "Classic"
+  )
+
+  dark <- grepl(
+    paste(
+      "ambiance",
+      "chaos",
+      "clouds midnight",
+      "cobalt",
+      "idle fingers",
+      "kr theme",
+      "material",
+      "merbivore soft",
+      "merbivore",
+      "mono industrial",
+      "monokai",
+      "pastel on dark",
+      "solarized dark",
+      "tomorrow night blue",
+      "tomorrow night bright",
+      "tomorrow night 80s",
+      "tomorrow night",
+      "twilight",
+      "vibrant ink",
+      sep = "|"
+    ),
+    editor,
+    ignore.case = TRUE)
+  dark <- !identical(global, "Classic") && dark
 
   list(
     editor = editor,
-    global = switch(
-      if (is.null(global)) "" else global,
-      alternate = "Sky",
-      default = "Modern",
-      "Classic"
-    )
+    global = global,
+    dark = dark
   )
 }
