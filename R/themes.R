4d70273cc23f55ad209f162c25645c8806a51c96
Javier Luraschi <javierluraschi@hotmail.com>
2017-07-07 14:24:14 -0700

    move function definition to .rs.api.getthemeinfo

--- R/themes.R
+++ R/themes.R
@@ -6,47 +6,6 @@
 #' the user interface is optimized for dark themes.
 #'
 #' @export
-getThemeInfo <- function(title, message, url = NULL) {
-  editor <- .rs.readUiPref("theme")
-
-  global <- .rs.readUiPref("flat_theme")
-  global <- switch(
-    if (is.null(global)) "" else global,
-    alternate = "Sky",
-    default = "Modern",
-    "Classic"
-  )
-
-  dark <- grepl(
-    paste(
-      "ambiance",
-      "chaos",
-      "clouds midnight",
-      "cobalt",
-      "idle fingers",
-      "kr theme",
-      "material",
-      "merbivore soft",
-      "merbivore",
-      "mono industrial",
-      "monokai",
-      "pastel on dark",
-      "solarized dark",
-      "tomorrow night blue",
-      "tomorrow night bright",
-      "tomorrow night 80s",
-      "tomorrow night",
-      "twilight",
-      "vibrant ink",
-      sep = "|"
-    ),
-    editor,
-    ignore.case = TRUE)
-  dark <- !identical(global, "Classic") && dark
-
-  list(
-    editor = editor,
-    global = global,
-    dark = dark
-  )
+getThemeInfo <- function() {
+  callFun("getThemeInfo")
 }
