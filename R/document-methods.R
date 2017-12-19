ee2e44c7309a1a7c00be34aa45ca1ac47097e071
AshesITR <rpgxpakt@yahoo.de>
2017-12-19 15:35:19 +0100

    Actually fix getSourceEditorContext()
    Makes rstudioapi::getSourceEditorContext() return NULL if there is no open source editor pane.
    The previous fix just changed the error message (now the failing line is as.document_selection(NULL) throwing Error: 'x' should be a list of {range, text} pairs)

--- R/document-methods.R
+++ R/document-methods.R
@@ -213,12 +213,11 @@ primary_selection.document_selection <- function(x, ...) {
 getDocumentContext <- function(fn) {
   context <- callFun(fn)
 
-  if (is.character(context$path))
-    Encoding(context$path) <- "UTF-8"
-
-  if (is.character(context$contents))
-    Encoding(context$contents) <- "UTF-8"
-
+  if (is.null(context)) return(NULL)
+  
+  Encoding(context$path) <- "UTF-8"
+  Encoding(context$contents) <- "UTF-8"
   context$selection <- as.document_selection(context$selection)
+  
   structure(context, class = "document_context")
 }
