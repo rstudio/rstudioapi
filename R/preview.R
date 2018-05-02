aee76014e9ce83100fcce372ce69fd7055d75507
Javier Luraschi <javierluraschi@hotmail.com>
2018-05-01 19:44:47 -0700

    improve and fix sql preview docs

--- R/preview.R
+++ R/preview.R
@@ -1,14 +1,14 @@
 #' Preview SQL statement
 #'
-#' Makes use of 'DBI' and \code{dbGetQuery()} to execute for proview
-#' a SQL statement for a given 'DBI' connection.
+#' Makes use of 'DBI' and \code{dbGetQuery()} to preview a SQL statement
+#' for a given 'DBI' connection.
 #'
 #' @param conn The 'DBI' connection to be used to execute this statement.
-#' @param statement The SQL stament to execute. Either a path to a
+#' @param statement The SQL statement to execute. Either a path to a
 #'   file containing a SQL statement or the SQL statement itself.
 #' @param ... Additional arguments to be used in \code{dbGetQuery()}.
 #'
-#' Available from RStudio 1.2.600.
+#' @note The \code{previewSql} function was introduced in RStudio 1.2.600
 #'
 #' @export
 previewSql <- function(conn, statement, ...) {
