1c8045897c0b9ecf1027e124bb1ed8639f937e5f
Kevin Ushey <kevinushey@gmail.com>
2018-09-07 09:21:23 -0700

    provide 'buildToolsExec()' helper for completeness

--- R/build-tools.R
+++ R/build-tools.R
@@ -1,6 +1,6 @@
 #' Build Tools
 #'
-#' Check and install build tools as required.
+#' Check, install, and use build tools as required.
 #'
 #' These functions are intended to be used together -- one should
 #' first check whether build tools are available, and when not,
@@ -8,10 +8,17 @@
 #'
 #' ```R
 #' compile_model <- function(...) {
-#'   if (!rstudioapi::buildToolsCheck())
-#'     rstudioapi::buildToolsInstall("Model compilation")
 #'
-#'   # proceed with compilation
+#'   if (rstudioapi::isAvailable()) {
+#'
+#'     if (!rstudioapi::buildToolsCheck())
+#'       rstudioapi::buildToolsInstall("Model compilation")
+#'
+#'     rstudioapi::buildToolsExec({
+#'       # code requiring build tools here
+#'     })
+#'
+#'   }
 #' }
 #' ```
 #'
@@ -22,8 +29,11 @@
 #' @param action The action (as a string) being taken that will require
 #'   installation of build tools.
 #'
-#' @note The `buildToolsCheck()` and `buildToolsInstall()` functions were added
-#'   with version 1.2.945 of RStudio.
+#' @param expr An \R expression (unquoted) to be executed with build tools
+#'   available and on the `PATH`.
+#'
+#' @note The `buildToolsCheck()`, `buildToolsInstall()`, and `buildToolsExec()`
+#'   functions were added with version 1.2.962 of RStudio.
 #'
 #' @name build-tools
 NULL
@@ -36,4 +46,9 @@ buildToolsCheck <- function() {
 #' @name build-tools
 buildToolsInstall <- function(action) {
   callFun("buildToolsInstall", action)
+}
+
+#' @name build-tools
+buildToolsExec <- function(expr) {
+  callFun("buildToolsExec", expr)
 }
