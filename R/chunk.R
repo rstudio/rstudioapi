0b2f56c4a86e11810afa1d171655bd06bb6b9bea
Melissa Barca <5323711+melissa-barca@users.noreply.github.com>
2020-09-11 13:56:59 -0400

    Apply suggestions from code review
    Co-authored-by: Kevin Ushey <kevin@rstudio.com>

--- R/chunk.R
+++ R/chunk.R
@@ -1,9 +1,9 @@
 #' Register Chunk Callback
 #'
-#' Registers a callback function to be executed after a chunk within an R Markdown document is run
+#' Registers a callback function to be executed after a chunk within an R Markdown document is run.
 #'
 #' @param callback A callback function that returns a list of html output to be
-#' displayed after a chunk is executed. The callback will be passed the parameter chunkName and chunkCode.
+#' displayed after a chunk is executed. The callback will be passed two parameters; `chunkName` (referring to the chunk label) and `chunkCode` (the code within the chunk).
 #' @return A handle that can be used to unregister the chunk.
 #' @export
 registerChunkCallback <- function(callback)
@@ -15,7 +15,7 @@ registerChunkCallback <- function(callback)
 #'
 #' Unregister a chunk callback previously registered via `registerChunkCallback()`.
 #'
-#' @param id The handle returned when the callback was registered
+#' @param id A handle, as returned via a previous call to [registerChunkCallback].
 #' @export
 unregisterChunkCallback <- function(id)
 {
